a823852e1903969d34f4424ffe435caf56400f1f
hugoprferreira <hugoprferreira@gmail.com>
2023-03-14 08:57:22 -0300

    Initial commit

---
+++ Ignite/-Fundamentos-React/src/App.jsx
@@ -0,0 +1,11 @@
+import { Post } from "./Post";
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <Post author="Diego Fernandes" content="Lorem ipsum olor" />;
+      <Post author="Gabriel Buzzi" content="Lorem ipsum olor" />;
+    </div>
+  );
+}
