772e2e88d0118abe52655e7f56cf8b901540b13d
hugoprferreira <hugoprferreira@gmail.com>
2023-04-01 06:55:22 -0300

    Projeto 02 pimeiro modulo

--- Ignite/02-Pomodoro/src/App.tsx
+++ Ignite/02-Pomodoro/src/App.tsx
@@ -1,12 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { BrowserRouter } from "react-router-dom";
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/themes/global";
+import { Router } from "./Router";
 
 export function App() {
   return (
-    <>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
