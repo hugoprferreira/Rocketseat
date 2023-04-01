348099534d62aa0aae0dcf1701a5d8d0eefaf8ec
hugoprferreira <hugoprferreira@gmail.com>
2023-04-01 18:52:25 -0300

    Componente: status

---
+++ Ignite/02-Pomodoro/src/pages/History/index.tsx
@@ -0,0 +1,47 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Historico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duracao</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
