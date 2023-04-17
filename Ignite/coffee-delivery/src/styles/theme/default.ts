b7f2ed300008521d330dcf972240e0b978c21626
hugoprferreira <hugoprferreira@gmail.com>
2023-04-17 16:10:55 -0300

    coffee delivery

---
+++ Ignite/coffee-delivery/src/styles/theme/default.ts
@@ -0,0 +1,16 @@
+export const defaultTheme = {
+  colors: {
+    base_white: "#FFF",
+
+    base_background: "#FAFAFA",
+    base_button: "#E6E5E5",
+    base_hover: "#D7D5D5",
+    base_subtitle: "#403937",
+    base_titte: "#27221",
+
+    products_purple: "#8047f8",
+    products_purpledark: "#4B2995",
+    products_yellow: "#DBAC2C",
+    products_yellowdark: "#C47F17",
+  },
+};
